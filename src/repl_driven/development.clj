9e75fa57eef52edc79839dd6d2f16ed990f4c912
Jorge Zaccaro <boolpath@192.168.1.106>
2023-04-15 15:13:41 +0100

    clojure -Tnew scratch

---
+++ src/repl_driven/development.clj
@@ -0,0 +1,12 @@
+(ns repl-driven.development
+  "clojure -Tnew scratch :name repl-driven :scratch repl-driven/development")
+
+(defn exec
+  "clojure -X repl-driven.development/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "clojure -M -m repl-driven.development"
+  [& args]
+  (println "-main with" args))
